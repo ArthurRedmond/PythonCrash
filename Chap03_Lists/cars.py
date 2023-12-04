f10b1d3d9a86a5e32904a4770819da95aab49f1b
Arthur Redmond <19720427+ArthurRedmond@users.noreply.github.com>
2023-12-04 16:36:47 -0500

    chapter 3

---
+++ Chap03_Lists/cars.py
@@ -0,0 +1,14 @@
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print(cars)
+cars.sort()
+print(cars)
+cars.sort(reverse=True)
+print(cars)
+
+cars = ['bmw', 'audi', 'toyota', 'subaru']
+print("Here's the original list:")
+print(cars)
+print("\nHere is the sorted list:")
+print(sorted(cars))
+print("\nHere is the original list again:")
+print(cars)
